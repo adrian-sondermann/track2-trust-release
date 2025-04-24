f74714278ee21f58eaafeb8ad5b20be997488185
Ramona Bei der Kellen <ramona.bei-der-kellen@adesso.de>
2025-04-24 10:26:52 +0000

    updated requirements and fixed warning in sidebar

--- sidebar.py
+++ sidebar.py
@@ -5,5 +5,5 @@ def render_sidebar_logo():
     """Render the logo in the sidebar from the static images folder using the working directory."""
     BASE_DIR = os.getcwd()
     image_path = os.path.join(BASE_DIR, "static", "images", "logo.png")
-    st.sidebar.image(image_path, use_column_width=True)
+    st.sidebar.image(image_path, use_container_width=True)
 
